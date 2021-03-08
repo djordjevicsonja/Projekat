ebf1b9d5a80c9ef9473fee44545de4e4239e0ed2
Sonja <djordjevicsonja@outlook.com>
2021-03-08 01:50:59 +0100

    proizvodi i priprema korpe

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,11 @@ const navList = document.querySelector("#header-nav");
 const search = document.getElementById("search");
 const searchIcon = document.querySelector(".search-icon");
 const selectByCategory = document.querySelector('#selectByCategory');
+const selectByPrice = document.querySelector('#selectByPrice');
+const closeCart = document.querySelector('.close-cart');
+const cartBtn = document.querySelector('.cart-btn');
+const cartDOM = document.querySelector('#cart');
+let cart=[];
 
 class AllProducts {
   async getAllProducts() {
@@ -50,8 +55,12 @@ class View {
    
     console.log(data);
     this.getCategoryBtns();
-    this.getProducts(data);
-    this.selectCategory(data);
+    this.getProductsFromCategory(data);
+
+    if(window.location.href.endsWith('proizvodi.html')){
+      this.selectCategory(data);
+      this.filterByPrice(data);
+    }
   }
 
   getCategoryBtns() {
@@ -65,30 +74,31 @@ class View {
       });   
   }
   
-  getProducts(data){
+  getProductsFromCategory(data){
     let getId = Storage.getCategory();
     console.log(getId);
    console.log(data);
    let categProd = data.filter(prod => prod.categoryId == getId);
    console.log(categProd);
-   this.renderProducts(categProd);
-    
-  }
+   this.renderProducts(categProd);    
+}
 
-  renderProducts(prodCateg) {
+  renderProducts(prodCateg){
     let output = "";
     for (let i in prodCateg) {
       for (let j in prodCateg[i].articles) {
         output += `
             <div class="img-item-container" data-id="${prodCateg[i].categoryId}">
+            <div class="img-cart-container">
             
             <img src="${prodCateg[i].articles[j].image.src}" alt="${prodCateg[i].articles[j].image.alt}" class="img-fluid img-item">
-            
+            <button class="addToCart"><i class="fas fa-shopping-cart"></i></button>
+            </div>            
             <ul>
             <li>${prodCateg[i].articles[j].article}</li>
             <li>${prodCateg[i].articles[j].manifactur}</li>
             <li>${prodCateg[i].articles[j].dimension}</li>
-            <li>${prodCateg[i].articles[j].price}</li>
+            <li>${prodCateg[i].articles[j].price} RSD</li>
             </ul>
             <button data-id="${prodCateg[i].articles[j].id}"class="seeProduct">Pogledaj proizvod</button>
             </div>
@@ -108,37 +118,81 @@ class View {
         btn.addEventListener("click", () => {
           let btnId = btn.dataset.id;
           console.log(btnId);
-          // Storage.category(btnId);
-          // window.location.href = 'proizvodi.html';
+          Storage.product(btnId);
+          window.location.href = 'proizvod.html';
           });
         });
     };
 
-
   selectCategory(data){
     let categorySelect = data.map(el => el.category);
-    console.log(categorySelect);
-    this.optionCateg(categorySelect);
-  }
-  optionCateg(categorySelect){
-    
-      let ispis = '';
+    console.log(categorySelect);  
+      let output = '';
       for(let i=0; i<categorySelect.length; i++){
-        ispis += `
+        output += `
         <option value="${i}">${categorySelect[i]}</option>
         `;
       }
-        selectByCategory.innerHTML=ispis;
+        selectByCategory.innerHTML=output; 
+        selectByCategory.selectedIndex = Storage.getCategory('category')-1;
+        if(!Storage.getCategory('category')){
+           selectByCategory.selectedIndex = 0;           
+        }
+        
+        selectByCategory.addEventListener('change',()=>{
+          let cat = selectByCategory.options[selectByCategory.selectedIndex].value;
+          let selectedCat = parseInt(cat) + 1
+          console.log(selectedCat);
+          Storage.category(selectedCat);  
+          let setupProd = data.filter(prod => prod.categoryId == selectedCat);
+          this.renderProducts(setupProd);
+          console.log(setupProd);
+        })
 
-
-    
-   
   }
+  filterByPrice(data){
+  
+    selectByPrice.addEventListener('change',()=>{
+      let id = Storage.getCategory();
+      let productToSort = data.filter(prod=>{
+        if(prod.categoryId== id){
+         return prod.articles;
+        }
+      });
 
- 
+    let idx = selectByPrice.selectedIndex;
+   
+        if(idx=='1'){
+          productToSort.sort((a, b)=>{
+            if(a.price>b.price) return -1;
+            else if(a.price<b.price) return 1;
+            else return 0;
+          });
+          this.renderProducts(productToSort);
+        }else if(idx=='2'){
+         productToSort.sort((a,b)=>{
+           if(a.price>b.price) return 1;
+           else if(a.price<b.price) return -1;
+           else return 0;
+         });
+         this.renderProducts(productToSort);   
+        }    
+    });
+  }
 
   listeners() {
     this.showDropdownMenu();
+    this.displayCart();
+  }
+  showCart(){
+      cartDOM.classList.add('showCart');   
+  }
+  hideCart(){
+    cartDOM.classList.remove('showCart');
+  }
+  displayCart(){
+    cartBtn.addEventListener('click',this.showCart);
+    closeCart.addEventListener('click',this.hideCart);
   }
   showDropdownMenu() {
     dropdownIcon.addEventListener("click", () => {
@@ -149,8 +203,49 @@ class View {
   }
 }
 
-class Registration{
-  
+class Product {
+  allAboutProduct(data){
+    let productId = Storage.getProduct();
+    let categId =Storage.getCategory();
+    for(let i in data){
+      let categ = data[i];
+      if(categ.categoryId == categId){
+        for(let j in categ.articles){
+          let prod = categ.articles[j];
+          if(prod.id == productId){
+        this.renderProduct(prod);          
+          }
+        }
+      }
+    }   
+  }
+  renderProduct(prod){
+    const productRow = document.createElement("div");
+    productRow.className = "row";  
+     productRow.innerHTML=`
+    <div class="col-12 col-lg-6">
+    <div class="product-img">
+        <img src="${prod.image.src}" alt="${prod.image.alt}">
+    </div>                   
+</div>
+<div class="col-12 col-lg-6">
+    <div class="product-info">                       
+        <h2>${prod.article}</h2>
+        <h3>${prod.manifactur}</h3>
+        <h4>${prod.price}RSD</h4>
+        <button class="addProdToCart" data-id="${prod.id}">Dodaj u korpu</button>
+        <h5>Opis:</h5>
+        <p>${prod.desc}</p>
+        <h6><a href="proizvodi.html">Svi proizvodi iz kategorije</a></h6>
+    </div>                  
+</div>
+  `;
+    if(window.location.href.endsWith("proizvod.html")){
+   productBox.appendChild(productRow);
+    }
+    
+
+  }
 }
 
 class Storage {
@@ -160,19 +255,33 @@ class Storage {
   static getCategory() {
     return localStorage.getItem('category');
   }
+  static product(btnId){
+    localStorage.setItem('product',btnId);
+  }
+  static getProduct(){
+    return localStorage.getItem('product');
+  }
+  static saveCart(cart){
+    localStorage.setItem('cart',JSON.stringify(cart));
+  }
+  static getCart(){
+    return localStorage.getItem('cart')?JSON.parse(localStorage.getItem('cart')):[];
+  }
 
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const products = new AllProducts();
   const view = new View();
+  const p = new Product();
+
+  view.listeners();
   products
     .getAllProducts()
     .then((data) => {
       view.renderCategories(data);
-    })
-
-    .then(() => {
-      view.listeners();
+      p.allAboutProduct(data);
     });
+
+    
 });
